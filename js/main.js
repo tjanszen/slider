b37548e74b434106bccd707ff59e980273e2edf1
Codective <codective@gmail.com>
2015-02-13 15:17:29 -0800

    Random start

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 $(function() {
-  var mySlider = $("input.slider").slider( { min: 0, max: 100, step: 5 } );
+  var mySlider = $("input.slider").slider( { min: 0, max: 100, step: 5, value: _.random(5, 95) } );
+  $('#slider-modal').modal({ backdrop: 'static', keyboard : false, show: true });
 });
